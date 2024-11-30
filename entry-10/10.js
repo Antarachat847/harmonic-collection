7d6c6c5a20587df79355c054b97fa4ab82bbb6a3
Antarachat847 <chata416@newschool.edu>
2024-11-29 22:55:57 -0500

    kmscdjon

--- entry-10/10.js
+++ entry-10/10.js
@@ -2,5 +2,6 @@
 window.addEventListener('scroll', function () {
   const gallery = document.querySelector('.gallery');
   let scrollPosition = window.pageYOffset;
-  gallery.style.transform = `translateY(${scrollPosition * 0.5}px)`; // Parallax scroll effect
+  gallery.style.transform = `translateY(${scrollPosition * 0.5}px)`; 
+  
 });
